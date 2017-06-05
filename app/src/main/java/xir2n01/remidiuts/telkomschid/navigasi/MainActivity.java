6a2f344acb5b7ee3bcf78eec77bf81197df9eaaa
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2017-06-05 13:41:12 +0700

    Second commit : MainActivity, Layout, Adding some Activity

--- app/src/main/java/xir2n01/remidiuts/telkomschid/navigasi/MainActivity.java
+++ app/src/main/java/xir2n01/remidiuts/telkomschid/navigasi/MainActivity.java
@@ -1,7 +1,10 @@
 package xir2n01.remidiuts.telkomschid.navigasi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnNav1 = (Button) findViewById(R.id.btnNav1);
+        Button btnNav2 = (Button) findViewById(R.id.btnNav2);
+        Button btnNav3 = (Button) findViewById(R.id.btnNav3);
+        Button btnNav4 = (Button) findViewById(R.id.btnNav4);
+        Button btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnNav1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindah = new Intent(MainActivity.this, Nav1Activity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnNav2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindah = new Intent(MainActivity.this, Nav2Activity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnNav3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindah = new Intent(MainActivity.this, Nav3Activity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnNav4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindah = new Intent(MainActivity.this, Nav4Activity.class);
+                startActivity(pindah);
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveTaskToBack(true);
+            }
+        });
     }
 }
