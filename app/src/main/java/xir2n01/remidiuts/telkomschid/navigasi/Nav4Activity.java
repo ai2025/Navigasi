c876aba9b6252b9ff9f724e2fdd14d65b2f3295f
Aisyatush Shofiah <aisyatushshofiah@gmail.com>
2017-06-06 20:07:01 +0700

    Nav4 Commit

--- app/src/main/java/xir2n01/remidiuts/telkomschid/navigasi/Nav4Activity.java
+++ app/src/main/java/xir2n01/remidiuts/telkomschid/navigasi/Nav4Activity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -40,6 +41,9 @@ public class Nav4Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_gallery);
+        navigationView.setCheckedItem(R.id.nav_gallery);
     }
 
     @Override
@@ -78,24 +82,29 @@ public class Nav4Activity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        changePage(id);
 
-        } else if (id == R.id.nav_slideshow) {
+        return true;
+    }
 
-        } else if (id == R.id.nav_manage) {
+    private void changePage(int id) {
 
-        } else if (id == R.id.nav_share) {
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_gallery) {
+            fragment = new Nav4F1Fragment();
+            setTitle("Foto Diri");
+        } else if (id == R.id.nav_data) {
+            fragment = new Nav4F2Fragment();
+            setTitle("Data Diri");
         }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commitNow();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
